caee5da6ffa7187e549260ed408c9b6267bb5067
micaelaruiz1 <micaelaruiz@udesa.edu.ar>
2026-04-22 17:10:08 -0300

    arregle error en el metodo de register y header de index

--- pi_progra2_grupo2/controllers/indexController.js
+++ pi_progra2_grupo2/controllers/indexController.js
@@ -3,7 +3,7 @@ const data = require("../db/index");
 const controller = {
     index: function (req, res) {
         let usuario = data.usuario;
-        return  res.render('index', {products: data.lista, usuario: usuario, logueado: true});
+        return  res.render('index', {products: data.lista, usuario: usuario, logueado: false});
     }
 }
 
