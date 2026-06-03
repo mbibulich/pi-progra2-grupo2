ca53884124c3d73b4cae0c19cb3f33d871a4f8f1
halleymoneta <hmonetajomaa@udesa.edu.ar>
2026-06-03 17:56:59 -0300

    borre lineas

--- pi_progra2_grupo2/routes/users.js
+++ pi_progra2_grupo2/routes/users.js
@@ -8,9 +8,5 @@ router.post('/register', userController.processRegister);
 router.get('/login', userController.login);
 router.post('/login', userController.processLogin);
 router.get('/profile', userController.profile);
-router.get('/edit/:id', userController.edit);
-router.post('/edit/:id', userController.update);
-router.get('/logout', userController.logout);
-router.post('/delete/:id', userController.delete);
 
 module.exports = router;
