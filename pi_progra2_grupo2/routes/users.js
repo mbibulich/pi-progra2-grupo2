32fa9706f6a2dd7fffe6548e55847bfd6f247b1d
halleymoneta <hmonetajomaa@udesa.edu.ar>
2026-06-08 17:55:21 -0300

    hice validacion del formulario de register y login. Modifique los headers en las vistas. Resolvi conflictos del nombre que estaba en el alias. Instale bcrypt y importe bcrypt y la db en ruteador de usuaios

--- pi_progra2_grupo2/routes/users.js
+++ pi_progra2_grupo2/routes/users.js
@@ -1,12 +1,51 @@
 var express = require('express');
 var router = express.Router();
 const userController = require("../controllers/userController");
+const db = require("../db/models");
+const bcrypt = require('bcryptjs');
+const { body } = require('express-validator');
+
+let validations = [
+    body('name')
+        .notEmpty().withMessage('Debes completar el nombre').bail()
+        .isLength({ min: 3, max: 50 }).withMessage('El nombre debe tener entre 3 y 50 caracteres'),
+    body('email')
+        .notEmpty().withMessage('Debes completar el email').bail()
+        .isEmail().withMessage('Debe ingresar un email válido')
+        .custom(function(value) {
+            return db.Usuario.findOne({ 
+                where: { email: value } 
+            })
+            .then(function(user) {
+                if (user) { 
+                    throw new Error('El email ingresado ya existe.');
+                }
+            });
+    }),
+    body('password')
+        .notEmpty().withMessage('Debes completar la contraseña').bail()
+        .isLength({ min: 3 }).withMessage('La contraseña debe tener al menos 3 caracteres')
+        .custom(function(value, { req }) {
+            return db.Usuario.findOne({ 
+                where: { email: req.body.email } 
+            })
+            .then(function(user) {
+                if (user) { 
+                    if (!bcrypt.compareSync(value, user.password)) {
+                        throw new Error('La contraseña es incorrecta.');
+                    }
+                }
+            });
+    }) 
+]
 
 /* GET users listing. */
 router.get('/register', userController.register);
-router.post('/register', userController.processRegister);
+router.post('/register', validations, userController.processRegister);
+
 router.get('/login', userController.login);
-router.post('/login', userController.processLogin);
+router.post('/login', validations, userController.processLogin);
+
 router.get('/profile', userController.profile);
 
 module.exports = router;
